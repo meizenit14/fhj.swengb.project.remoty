7d24e3d2f069d754756c28523fc7973057af0a7b
Amar Bajric <amar.bajric78@gmail.com>
2015-12-19 19:19:16 +0100

    initial commit

---
+++ src/main/scala/fhj/swengb/project/remoty/remotyPlaceholder.scala
@@ -0,0 +1,8 @@
+package fhj.swengb.project.remoty
+
+
+object remotyPlaceholder  {
+
+  //insert here the engine of the remoty app
+
+}
---
+++ src/test/scala/fhj/swengb/project/remoty/RemotyTest.scala
@@ -0,0 +1,17 @@
+package fhj.swengb.project.remoty
+
+import org.junit._
+
+/**
+  * Created by Amar on 19.12.2015.
+  *
+  * Write tests here in the remotyTest class
+  */
+
+class RemotyTest {
+
+  @Test def sampleTest(): Unit = {
+    assert(true)
+  }
+
+}
