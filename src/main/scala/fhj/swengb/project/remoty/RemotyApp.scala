486a0111aa749b56edfca051f88fb83c1385cd5a
Amar Bajric <amar.bajric78@gmail.com>
2015-12-26 14:18:01 +0100

    test of TreeView works

--- src/main/scala/fhj/swengb/project/remoty/RemotyApp.scala
+++ src/main/scala/fhj/swengb/project/remoty/RemotyApp.scala
@@ -1,11 +1,13 @@
 package fhj.swengb.project.remoty
 
+
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.application.Application
-import javafx.fxml.{Initializable, FXMLLoader}
-import javafx.scene.control.TreeView
-import javafx.scene.layout.BorderPane
+import javafx.fxml.{FXML, Initializable, FXMLLoader}
+import javafx.scene.control.{TreeView,TreeItem}
+import javafx.scene.image.{ImageView, Image}
+import javafx.scene.layout.{Pane, StackPane, BorderPane}
 import javafx.scene.{Scene, Parent}
 import javafx.stage.Stage
 
@@ -49,12 +51,32 @@ class RemotyApp extends javafx.application.Application {
 
 class RemotyAppController extends Initializable {
 
-  var border_pane: BorderPane = _
+  @FXML var pane_view: Pane = _
+  @FXML var tree_view: TreeView[String] = _
 
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    //set the rootItem to the tree_view
+    tree_view.setRoot(rootItem)
+
 
   }
+
+  //set a value for the picture of an folder Icon and use it for TreeItems
+  val picture: Image = new Image("/fhj/swengb/project/remoty/genericFolderYellow.png")
+
+
+  //make a root
+  //second argument in TreeItem is a new ImageView with the "picture" value in it and then it will show an folder icon in the treeview
+  val rootItem: TreeItem[String] = new TreeItem("Root",new ImageView(picture))
+
+  //make subroots
+  val item: TreeItem[String] = new TreeItem[String]("Subroot")
+  //add subroots to rootItem
+  rootItem.getChildren.addAll(item)
+
+
+
 
 
 
