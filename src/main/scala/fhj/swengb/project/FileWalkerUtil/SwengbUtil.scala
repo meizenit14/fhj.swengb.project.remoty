bca4fb0615489b08a5cc1aa271fd2c85d57e8e53
Amar Bajric <amar.bajric78@gmail.com>
2016-01-15 09:11:59 +0100

    file walker added

---
+++ src/main/scala/fhj/swengb/project/FileWalkerUtil/SwengbUtil.scala
@@ -0,0 +1,66 @@
+package fhj.swengb.project.FileWalkerUtil
+
+import java.io.{File, IOException}
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.nio.file._
+import java.nio.file.attribute.BasicFileAttributes
+
+import scala.io.Source
+
+/**
+  * Useful tooling for basic operations
+  */
+object SwengbUtil {
+
+  def mkParent(file : File) : Unit = {
+    if (!file.getParentFile.exists()) {
+      file.getParentFile.mkdirs()
+    }
+  }
+  def writeToFile(file: File, content: String): File = {
+    Files.write(Paths.get(file.toURI), content.getBytes(StandardCharsets.UTF_8)).toFile
+  }
+
+  def fetch(url: URL): String = {
+    Source.fromURL(url).mkString
+  }
+
+  /**
+    * function to measure execution time of first function, optionally executing a display function,
+    * returning the time in milliseconds
+    */
+  def time[A](a: => A, display: Long => Unit = s => ()): A = {
+    val now = System.nanoTime
+    val result = a
+    val micros = (System.nanoTime - now) / 1000000
+    display(micros)
+    result
+  }
+
+  def removeRecursive(path: Path): Unit = {
+    Files.walkFileTree(path, new SimpleFileVisitor[Path]() {
+      override def visitFile(path: Path, attrs: BasicFileAttributes): FileVisitResult = {
+        Files.delete(path)
+        FileVisitResult.CONTINUE
+      }
+
+      override def visitFileFailed(path: Path, exc: IOException): FileVisitResult = {
+        Files.delete(path)
+        FileVisitResult.CONTINUE;
+      }
+
+      override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult = {
+        if (exc == null) {
+          Files.delete(dir)
+          FileVisitResult.CONTINUE
+        } else {
+          throw exc
+        }
+      }
+    }
+    )
+  }
+}
+
+
