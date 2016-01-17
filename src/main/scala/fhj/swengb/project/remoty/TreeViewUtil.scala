bb066565228a9e36bf8b8a541663ab2cba381836
Amar Bajric <amar.bajric78@gmail.com>
2016-01-17 11:31:44 +0100

    added new File for TreeView stuff

---
+++ src/main/scala/fhj/swengb/project/remoty/TreeViewUtil.scala
@@ -0,0 +1,14 @@
+package fhj.swengb.project.remoty
+
+/**
+  * Created by Amar on 15.01.2016.
+  */
+object TreeViewUtil {
+
+
+  /**
+    * Here comes the TreeView thing with observables and cell factories
+    * Coming soon...
+    */
+
+}
